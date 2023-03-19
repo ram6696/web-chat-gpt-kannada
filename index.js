8b79483397791050eeef48e3b2ab8729c64a1dbe
Sriram <sriram@nineleaps.com>
2023-03-19 15:46:43 +0530

    Bot replies conversation working

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ const {Translate} = require('@google-cloud/translate').v2;
 
 // Import other required libraries
 const fs = require('fs');
-const util = require('util');
+const path = require('path');
+
+const CONVERSATIONS_JSON = path.join(__dirname, 'conversations.json');
 
 const client = new textToSpeech.TextToSpeechClient();
 const translate = new Translate();
@@ -33,6 +35,18 @@ const server = app.listen(process.env.PORT || 5000, () => {
 const io = require('socket.io')(server);
 io.on('connection', function(socket){
   console.log('a user connected');
+  fs.readFile(CONVERSATIONS_JSON, function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    let conversations = [];
+    if(data.toString()) {
+      console.log('coming inside', data.toString());
+      conversations = JSON.parse(data.toString());
+    }
+    socket.emit('conversations', conversations);
+  });
 });
 
 // const apiai = require('apiai')(APIAI_TOKEN);
@@ -45,15 +59,6 @@ app.get('/', (req, res) => {
 io.on('connection', function(socket) {
   socket.on('chat message', async (text) => {
     console.log('Message: ' + text);
-
-    // convert text to english
-
-    // Get a reply from API.ai
-
-    // let apiaiReq = apiai.textRequest(text, {
-    //   sessionId: APIAI_SESSION_ID
-    // });
-
     const [englishTranslation] = await translate.translate(text, 'en-US');
     console.log('englishTranslation', englishTranslation);
 
@@ -79,26 +84,51 @@ io.on('connection', function(socket) {
   
       // Performs the text-to-speech request
       const [response] = await client.synthesizeSpeech(request);
+      const buff = Buffer.from(response.audioContent, 'base64');
+      fs.writeFileSync('output.mp3', buff);
+      fs.readFile(CONVERSATIONS_JSON, function(err, data) {
+        if (err) {
+          console.error(err);
+          process.exit(1);
+        }
+        let conversations = [];
+        if(data.toString()) {
+          console.log('coming inside', data.toString());
+          conversations = JSON.parse(data.toString());
+        }
+        if(conversations.length) {
+          // push user input
+          conversations.push(
+            {"chat": text, "buffer": "", "isBot": false}
+          );
+          conversations.push({
+            "chat": kannadaTranslation,
+            "buffer": response.audioContent,
+            "isBot": true
+          })
+        } else {
+          conversations = [
+            {"chat": text, "buffer": "", "isBot": false},
+            {
+              "chat": kannadaTranslation,
+              "buffer": response.audioContent,
+              "isBot": true
+            }
+          ]
+        }
+        fs.writeFile(CONVERSATIONS_JSON, JSON.stringify(conversations), function(err) {
+          if (err) {
+            console.error(err);
+            process.exit(1);
+          }
+          // Emits updated bid to all sockets upon successful save
+          socket.emit('conversations', conversations);	    
+        });
+      });
       socket.emit('audio reply', response.audioContent)
       socket.emit('bot reply', kannadaTranslation);
     } catch (error) {
       console.log(error);
     }
-    
-
-    
-
-    // apiaiReq.on('response', (response) => {
-    //   let aiText = response.result.fulfillment.speech;
-    //   console.log('Bot reply: ' + aiText);
-    //   socket.emit('bot reply', aiText);
-    // });
-
-    // apiaiReq.on('error', (error) => {
-    //   console.log(error);
-    // });
-
-    // apiaiReq.end();
-
   });
 });
